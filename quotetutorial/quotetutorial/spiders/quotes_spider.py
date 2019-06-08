4894c0451dcc1f99a022700325630633a942b453
Elias <eeliasmaia@gmail.com>
2019-06-08 16:25:35 -0300

    Follow up of class 11 - Add a for loop and gets all title, author and tag information of all quotes in the page

--- quotetutorial/quotetutorial/spiders/quotes_spider.py
+++ quotetutorial/quotetutorial/spiders/quotes_spider.py
@@ -3,9 +3,18 @@ import scrapy
 class QuoteSpider(scrapy.Spider):
     name = 'quotes'
     start_urls = [
-    'http://quotes.toscrape.com/'
+        'http://quotes.toscrape.com/'
     ]
 
     def parse(self, response):
-        title = response.css('title::text').extract()
-        yield { 'titletext' : title }
+        all_div_quotes = response.css('div.quote')
+
+        for quotes in all_div_quotes:                
+            title = quotes.css('span.text::text').extract()
+            author = quotes.css('.author::text').extract()
+            tag = quotes.css('.tag::text').extract()
+            yield {
+                'title' : title,
+                'author' : author,
+                'tag' : tag
+            }
