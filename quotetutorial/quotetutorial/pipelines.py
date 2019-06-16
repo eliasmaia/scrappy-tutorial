87db4bcb0a2f5b04fb8fc1978b0dcc1ec1c8819d
Elias <eeliasmaia@gmail.com>
2019-06-16 16:07:29 -0300

    start using pipelines on web scraping

--- quotetutorial/quotetutorial/pipelines.py
+++ quotetutorial/quotetutorial/pipelines.py
@@ -5,7 +5,11 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
+# Scraped data -> Item containers -> Json/csv files
+# Scraped data -> Item Containers -> Pipeline -> SQL/Mongo database
 
 class QuotetutorialPipeline(object):
     def process_item(self, item, spider):
+
+        print("Pipeline :" + item['title'][0])
         return item
